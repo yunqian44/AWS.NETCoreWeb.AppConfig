05fa31a77fa8b9c9edaf0c974c0a6368e489bcb9
yunqian44 <yunqian8@live.com>
2021-03-31 16:57:54 +0800

    Overriding the connection string

--- AWS.NETCoreWeb.AppConfig/Data/UserContext.cs
+++ AWS.NETCoreWeb.AppConfig/Data/UserContext.cs
@@ -1,4 +1,7 @@
-﻿using AWS.NETCoreWeb.AppConfig.Model;
+﻿using AWS.NETCoreWeb.AppConfig.Common;
+using AWS.NETCoreWeb.AppConfig.Core;
+using AWS.NETCoreWeb.AppConfig.Model;
+using AWS.NETCoreWeb.AppConfig.Service;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -11,6 +14,8 @@ namespace AWS.NETCoreWeb.AppConfig.Data
 {
     public class UserContext : DbContext
     {
+        public IAppConfigDataService _appConfigDataService;
+
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
 
@@ -18,20 +23,20 @@ namespace AWS.NETCoreWeb.AppConfig.Data
 
         public DbSet<UserModel> Users { get; set; }
 
-        ///// <summary>
-        ///// 重写连接数据库
-        ///// </summary>
-        ///// <param name="optionsBuilder"></param>
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    // 从 appsetting.json 中获取配置信息
-        //    var config = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json")
-        //        .Build();
+        /// <summary>
+        /// 重写连接数据库
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 从 appsetting.json 中获取配置信息
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-        //    // 定义要使用的数据库
-        //    optionsBuilder.UseMySql("server=192.168.99.100;uid=root;pwd=qwer1234!;database=nationalday_ddd");
-        //}
+            // 定义要使用的数据库
+            optionsBuilder.UseMySql(Appsettings.app("MySql"));
+        }
     }
 }
